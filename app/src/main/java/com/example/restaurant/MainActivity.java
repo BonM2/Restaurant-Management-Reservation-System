1febf09152f99d107a857f66beb8a8e8f1d4fffe
Le Nguyen Quoc Bao <lenguyenquocbao2005@gmail.com>
2026-02-24 15:39:23 +0700

    Thay đổi UI của file home_layout

--- app/src/main/java/com/example/restaurant/MainActivity.java
+++ app/src/main/java/com/example/restaurant/MainActivity.java
@@ -14,6 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.profile_layout);
+        setContentView(R.layout.home_layout);
     }
 }
